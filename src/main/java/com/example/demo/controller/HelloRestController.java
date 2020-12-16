5e1539db9d7ff71258c4d99e934333395923956c
pallavikyama <kyamap98@gmail.com>
2020-12-16 23:17:10 +0530

    Added: Used a REST Controller to respond to a REST Call and used various qualifier values for request mapping a method.

---
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+	// curl localhost:8080/hello -w "\n"
+	@RequestMapping(value= {"","/","home"})
+	public String sayHello() {
+		return "Hello From BridgeLabz!!";
+	}
+	
+}
